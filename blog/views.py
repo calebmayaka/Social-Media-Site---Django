540b76e0865d2288403e8379e10c8096ecc66da5
Caleb Mayaka <mayakaombogo254@gmail.com>
2023-11-24 03:21:10 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 posts = [
     {
-        'author': 'CoreyMS',
+        'author': 'Cally',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'August 27, 2018'
+        'date_posted': 'August 27, 2023'
     },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
+        'date_posted': 'August 28, 2023'
     }
 ]
 
